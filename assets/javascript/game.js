e297ac5f7e84251d113d212f5f3502330fcf115b
Nathan Bernabe <nbernabe09@aol.com>
2017-07-04 23:27:38 -0700

    Updated game.js, closer to proper functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,29 @@
 window.onload = function() {
 	
+	var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 	var words = ["stitch", "mickey", "goofy", "pluto", "nemo"];
+	var usedChars = [];
 	var wordValue = words[Math.floor(Math.random() * words.length)];
-		wordValue = wordValue.replace(/\s/g, "-");
+	var maskedWord = wordValue.replace(/./g, "-");
 	var ranWord = document.getElementById("ran-word");
-		ranWord.textContent = wordValue;
+		ranWord.textContent = maskedWord;
+	console.log(wordValue);
 	var wins = 0;
-	
+		wins = document.getElementById("win-count");
+	var guesses = 10;
+		guesses = document.getElementById("guess-count");
+	var guessLetter = document.getElementById('user-guess');
+
 	document.onkeyup = function(event) {
-
-		var userGuess = document.getElementById("user-guess");
-			userGuess.textContent = event.key
-
+  	var key = event.key.toLowerCase();
+  	console.log(event.key)
+  	if (alphabet.indexOf(key) !== -1) {
+    	if (usedChars.indexOf(key) === -1) {
+      	usedChars.push(key);
+      	var string = usedChars.join(', ');
+      	guessLetter.textContent = string;
+    	};
+  	};
 	};
 
 };
