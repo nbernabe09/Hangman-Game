7a8a179e2586b49a3462f33b6cf22d0d1d2290c8
Nathan Bernabe <nbernabe09@aol.com>
2017-07-10 23:45:48 -0700

    Enhanced .js for better functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,77 @@
-window.onload = function() {
+window.onload = function bootup() {
 	
-	var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	var guesses = 10;
 	var words = ["stitch", "mickey", "goofy", "pluto", "nemo"];
+	var message = {
+		win: 'You win!',
+		lose: 'Ouch... try again.',
+	};
+
 	var usedChars = [];
+	var rightChars = [];
+	var shownLetter = 0;
+	var guessLetter = document.getElementById('user-guess');
+	document.getElementById("guess-count").textContent = guesses;
+	
 	var wordValue = words[Math.floor(Math.random() * words.length)];
 	var maskedWord = wordValue.replace(/./g, "-");
-	var ranWord = document.getElementById("ran-word");
-		ranWord.textContent = maskedWord;
+	document.getElementById("ran-word").innerHTML = maskedWord;
 	console.log(wordValue);
-	var wins = 0;
-		wins = document.getElementById("win-count");
-	var guesses = 10;
-		guesses = document.getElementById("guess-count");
-	var guessLetter = document.getElementById('user-guess');
+
+	// var wins = 0;
+	// document.getElementById("win-count").textContent = wins;
+
+	document.getElementById("message").textContent = "";
+	document.getElementById("user-guess").textContent = "...";
+
+	document.getElementById("restart").onclick = bootup;
+
+	function gameOver(win) {
+		if (win) {
+			// wins ++;
+			document.getElementById("message").textContent = message.win;
+			document.onkeyup = function() {
+  			return false;
+  		}
+		} else {
+			document.getElementById("message").textContent = message.lose;
+			document.onkeyup = function() {
+  			return false;
+  		}
+		}
+	};
 
 	document.onkeyup = function(event) {
   	var key = event.key.toLowerCase();
-  	console.log(event.key)
-  	if (alphabet.indexOf(key) !== -1) {
+  	console.log(key);
+  	if (wordValue.indexOf(key) > -1) {
+  		if (rightChars.indexOf(key) === -1) {
+  			rightChars.push(key);
+  			for (var i = 0; i < maskedWord.length; i++) {
+  				var letter = maskedWord[i]
+  				if (key.includes(wordValue[i])) {
+  					shownLetter ++;
+					}	
+  			}
+  		}
+
+  	} else if (alphabet.indexOf(key) !== -1) {
     	if (usedChars.indexOf(key) === -1) {
       	usedChars.push(key);
       	var string = usedChars.join(', ');
       	guessLetter.textContent = string;
-    	};
-  	};
-	};
+  			guesses --;
+  			document.getElementById("guess-count").textContent = guesses;
+    	}
+  	}
 
-};
+  	if (guesses === 0) {
+  		gameOver(false);
+  		document.getElementById("ran-word").innerHTML = wordValue;
+  	} else if (shownLetter === wordValue.length) {
+  		gameOver(true);
+  	}
 
-// function reset() {
-// 	location.reload(event);
-// };
+	}
+}
